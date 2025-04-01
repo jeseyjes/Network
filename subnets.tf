a59950a44dbf24673237878f03c151015821f907
jeseyjes <jessicathuo1@gmail.com>
2025-04-01 15:43:46 +0300

    fix Terraform formatting in subnets.tf and vpc.tf

--- subnets.tf
+++ subnets.tf
@@ -2,11 +2,11 @@
 resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
 
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnet_cidrs[count.index]
-  availability_zone = var.availability_zones[count.index]
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true # Required for public subnets
-  
+
   tags = {
     Name = "public-subnet-${count.index + 1}"
   }
@@ -19,7 +19,7 @@ resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_cidrs[count.index]
   availability_zone = var.availability_zones[count.index]
-  
+
   tags = {
     Name = "private-subnet-${count.index + 1}"
   }
--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
-  
+
   tags = {
     Name = "free-tier-vpc"
   }
@@ -10,7 +10,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-  
+
   tags = {
     Name = "free-tier-igw"
   }
