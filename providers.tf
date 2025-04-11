95be7d01bc0cbd23980f35632c3827e05e18ecf4
jeseyjes <jessicathuo1@gmail.com>
2025-04-11 08:05:54 +0300

    Initial Terraform VPC configuration

--- providers.tf
+++ providers.tf
@@ -11,4 +11,9 @@ terraform {
 provider "aws" {
   region = "eu-west-2"
 
+  # OIDC credentials will be automatically detected by Terraform Cloud
+  assume_role {
+    role_arn     = var.aws_role_arn
+    session_name = "terraform-cloud-session"
+  }
 }
